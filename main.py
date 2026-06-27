2b0a87179455fe774583d00f80d94f3d1e333907
shubhamss006 <shubhammahajan9444@gmail.com>
2026-06-27 14:13:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,24 +31,24 @@ your_choice= int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 fo
 if your_choice<=2 or your_choice>=0:
     print(image[your_choice])
 computer_choice =random.randint(0,2)
-print(f"computer choose :{computer_choice}")
+print(f"Computer choose :{computer_choice}")
 print(image[computer_choice])
 
 if your_choice==0 and computer_choice ==2:
-    print("you win!")
+    print("You win!")
 elif your_choice >= 3 or your_choice <0:
-    print("invalid input retry")
-elif computer_choice==0 and your_choice==2:
-    print("you loose")
-elif computer_choice > your_choice:
-    print("computer win")
-elif your_choice > computer_choice:
-    print("you win!")
-elif your_choice==0 and computer_choice== 1 :
-    print("you win")
-elif computer_choice==0 and your_choice ==1:
-    print("you loose")
-elif your_choice == computer_choice:
+    print("Invalid input retry")
+elif computer_chose==0 and your_chose==2:
+    print("You lose")
+elif computer_chose > your_chose:
+    print("Computer win")
+elif your_chose > computer_chose:
+    print("You win!")
+elif your_chose==0 and computer_chose== 1 :
+    print("You win")
+elif computer_chose==0 and your_chose ==1:
+    print("You lose")
+elif your_chosee == computer_chose:
     print("its tie play rematch")
 
 else :
